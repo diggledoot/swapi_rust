35c30c972f1f7e33f04481de2b5ede1b36f5fb56
Darren Chan <dfchan@idsdatanet.com>
2023-04-24 14:55:21 +0800

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,44 @@ const SWAPI_PEOPLE_URL: &str = "https://swapi.dev/api/people";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let raw_characters: Vec<RawCharacter> = get_characters(SWAPI_PEOPLE_URL.to_string()).await?;
-    let height_characters: Vec<RawCharacter> = get_characters_with_height(&raw_characters).unwrap();
-    let no_height_characters: Vec<RawCharacter> =
-        get_characters_with_no_height(&raw_characters).unwrap();
-    print_to_file("output.json", &raw_characters);
+    let genders: Vec<String> = extract_gender(&raw_characters).unwrap_or_else(|err| {
+        panic!("Failed to extract gender string!");
+    });
+    for gender in genders.iter() {
+        println!("{:?}", gender);
+    }
+    // let height_characters: Vec<RawCharacter> = get_characters_with_height(&raw_characters)
+    //     .unwrap_or_else(|err| {
+    //         panic!("Failed to extract characters with height!");
+    //     });
+    // let no_height_characters: Vec<RawCharacter> = get_characters_with_no_height(&raw_characters)
+    //     .unwrap_or_else(|err| {
+    //         panic!("Failed to extract characters with no height!");
+    //     });
+    // write_to_file("output.json", &raw_characters);
 
     Ok(())
 }
 
-fn print_to_file(file_name: &str, data: &Vec<RawCharacter>) {
-    let json = serde_json::to_string_pretty(&data).unwrap();
-    let mut file = File::create(file_name).unwrap();
-    file.write_all(json.as_bytes()).unwrap();
+fn extract_gender(characters: &[RawCharacter]) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut result: Vec<String> = Vec::new();
+    for character in characters.iter() {
+        if let Some(gender) = &character.gender {
+            if !result.contains(gender) {
+                result.push(gender.clone());
+            }
+        }
+    }
+    Ok(result)
+}
+
+fn write_to_file(file_name: &str, data: &Vec<RawCharacter>) {
+    let json: String = serde_json::to_string_pretty(&data)
+        .unwrap_or_else(|err| panic!("Failed to convert data to String!"));
+    let mut file: File =
+        File::create(file_name).unwrap_or_else(|err| panic!("Failed to create file!"));
+    file.write_all(json.as_bytes())
+        .unwrap_or_else(|err| panic!("Failed to write to file!"));
 }
 
 async fn get_characters(url: String) -> Result<Vec<RawCharacter>, Box<dyn std::error::Error>> {
@@ -30,9 +56,9 @@ async fn get_characters(url: String) -> Result<Vec<RawCharacter>, Box<dyn std::e
     let response: Response = reqwest::get(url).await?;
     let api_response: APIResponse = response.json().await?;
     result.extend(api_response.results);
-    let mut response_next = api_response.next;
+    let mut response_next: Option<String> = api_response.next;
     while let Some(url) = &response_next {
-        let response = reqwest::get(url).await?;
+        let response: Response = reqwest::get(url).await?;
         let api_response: APIResponse = response.json().await?;
         result.extend(api_response.results);
         response_next = api_response.next;
@@ -68,6 +94,6 @@ fn get_characters_with_no_height(
     Ok(result)
 }
 
-fn is_number(s: &str) -> bool {
-    s.parse::<i32>().is_ok()
+fn is_number(string: &str) -> bool {
+    string.parse::<i32>().is_ok()
 }
