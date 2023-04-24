dfd498240edd0f2e1983a30b5654fee6e1df3ae9
Darren Chan <dfchan@idsdatanet.com>
2023-04-24 08:05:17 +0800

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+use reqwest::Response;
 use swapi::models::{APIResponse, RawCharacter};
+
+const SWAPI_PEOPLE_URL: &str = "https://swapi.dev/api/people";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let response = reqwest::get("https://swapi.dev/api/people").await?;
-    let api_response: APIResponse = response.json().await?;
-
-    let mut raw_characters: Vec<RawCharacter> = Vec::new();
-
-    raw_characters.extend(api_response.results);
-
-    println!("{:?}", raw_characters);
+    let raw_characters: Vec<RawCharacter> = get_characters(SWAPI_PEOPLE_URL.to_string()).await?;
+    println!("{:?}", raw_characters.len());
     Ok(())
+}
+
+async fn get_characters(url: String) -> Result<Vec<RawCharacter>, Box<dyn std::error::Error>> {
+    let mut result: Vec<RawCharacter> = Vec::new();
+    let response: Response = reqwest::get(url).await?;
+    let api_response: APIResponse = response.json().await?;
+    result.extend(api_response.results);
+    let mut response_next = api_response.next;
+    while let Some(url) = &response_next {
+        let response = reqwest::get(url).await?;
+        let api_response: APIResponse = response.json().await?;
+        result.extend(api_response.results);
+        response_next = api_response.next;
+    }
+    Ok(result)
 }
